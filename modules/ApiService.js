ca63a47a0c57e17a0403850de8b829b8ffc9c1fa
Adrian Kowalski <adriann.kowalski@gmail.com>
2021-03-09 21:40:45 +0100

    Fix non-cors policy requirments

--- modules/ApiService.js
+++ modules/ApiService.js
@@ -1,6 +1,16 @@
 export const getWeatherByCity = city => { // fetching data by city name answer from API is ID of city
     return fetch(
-        `https://www.metaweather.com/api/location/search/?query=${city}` 
+        `https://www.metaweather.com/api/location/search/?query=${city}` ,
+        {
+              method: "POST",
+ headers: [
+  ['Content-Type', 'application/x-www-form-urlencoded'],
+  ['Content-Type', 'multipart/form-data'],
+  ['Content-Type', 'text/plain'],
+],
+  credentials: "include",
+  body: JSON.stringify(exerciseForTheReader)
+        }
     )
     .then(resp => resp.json())
     .then(data => { 
